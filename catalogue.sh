7924a8525ae031edd9ff01b854c353b18591ba45
raviteja <ravitejatfcdemo@gmail.com>
2023-01-05 14:57:27 +0530

    loaded schema and changes in catalogue.sh

--- catalogue.sh
+++ catalogue.sh
@@ -1,7 +1,5 @@
 repo_file=$(pwd)
 
-set -e
-
 # Setup NodeJS repos. Vendor is providing a script to setup the repos
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
@@ -11,7 +9,7 @@ yum install nodejs -y
 # Add application User
 # User roboshop is a function / daemon user to run the application.
 # Apart from that we dont use this user to login to server.
-#useradd roboshop
+useradd roboshop
 
 # Lets setup an app directory
 mkdir -p /app
@@ -27,8 +25,8 @@ unzip /tmp/catalogue.zip
 cd /app
 npm install
 
-# Setup the MongoDB repo file
-cp ${repo_file}/files/mongodb.repo /etc/yum.repos.d/mongo.repo
+# Setup SystemD Catalogue Service
+cp ${repo_file}/files/catalogue.service /etc/systemd/system/catalogue.service
 
 # Load the service
 systemctl daemon-reload
@@ -37,7 +35,12 @@ systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
 
-# Install MongoDB
+# We need to load the schema. To load schema we need to install mongodb client
+# To have it installed we can setup MongoDB repo and install mongodb-client
+cp ${repo_file}/files/mongodb.repo /etc/yum.repos.d/mongo.repo
 
+# Install MongoDB client
+yum install mongodb-org-shell -y
 
 # Load Schema
+mongo --host localhost </app/schema/catalogue.js
