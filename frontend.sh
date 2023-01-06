01b492f9648d96f46de93b9acc8232826fba1770
raviteja <ravitejatfcdemo@gmail.com>
2023-01-06 17:58:55 +0530

    applying if conditions

--- frontend.sh
+++ frontend.sh
@@ -4,36 +4,64 @@ LOG=/tmp/roboshop.log
 # Install Nginx
 echo -e "\e[35m Install Nginx\e[0m"
 yum install nginx -y &>>${LOG}
-echo $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
 
 # Remove the default content that web server is serving
 echo -e "\e[35m Remove Nginx Default Content\e[0m"
 rm -rf /usr/share/nginx/html/* &>>${LOG}
-echo $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
 
 # Download the frontend content
 echo -e "\e[35m Download The Front-End Content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
-echo $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
 
 # Extract the frontend content
 echo -e "\e[35m Extract The Front-End Content\e[0m"
 cd /usr/share/nginx/html &>>${LOG}
 unzip /tmp/frontend.zip &>>${LOG}
-echo $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
 
 # searching for files folder in /usr/share/nginx/html path, so we specify files folder location as variable
 
 echo -e "\e[35m Copy RoboShop Nginx Config File\e[0m"
 cp ${conf_file}/files/nginx_roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
-echo $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
 
 # Enable Nginx service
 echo -e "\e[35m Enable Nginx\e[0m"
 systemctl enable nginx &>>${LOG}
-echo $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
 
 # Restart Nginx Service to load the changes of the configuration
 echo -e "\e[35m Start Nginx\e[0m"
 systemctl restart nginx &>>${LOG}
-echo $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
