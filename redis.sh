eea506a544d15eab1e524eb1c9e768091b612393
raviteja <ravitejatfcdemo@gmail.com>
2023-01-08 12:18:25 +0530

    latest commit

--- redis.sh
+++ redis.sh
@@ -1,15 +1,15 @@
 source common.sh
 
 print_head "Setup Redis repo"
-yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y bash &>>${LOG}
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>${LOG}
 status_check
 
 print_head "Enable Redis 6.2"
-dnf module enable redis:remi-6.2 -y bash &>>${LOG}
+dnf module enable redis:remi-6.2 -y &>>${LOG}
 status_check
 
 print_head "Install Redis"
-yum install redis -y bash &>>${LOG}
+yum install redis -y &>>${LOG}
 status_check
 
 print_head "Update Listen Address"
@@ -17,9 +17,9 @@ sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${LOG}
 status_check
 
 print_head "Enable Redis Service"
-systemctl enable redis bash &>>${LOG}
+systemctl enable redis &>>${LOG}
 status_check
 
 print_head "Start Redis Service"
-systemctl start redis bash &>>${LOG}
+systemctl start redis &>>${LOG}
 status_check
