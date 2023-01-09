870d8854abf547b9ed0ed60e640c637d5d47d291
raviteja <ravitejatfcdemo@gmail.com>
2023-01-09 10:36:17 +0530

    latest commit

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,5 +1,10 @@
 source common.sh
 
+if [ -z "${roboshop_rabbitmq_password}" ]; then
+  echo "variable roboshop_rabbitmq_password needed"
+  exit
+fi
+
 print_head "Configure YUM Repos For ErLang"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash &>>${LOG}
 status_check
@@ -24,10 +29,18 @@ print_head "Start RabbitMQ Service"
 systemctl start rabbitmq-server &>>${LOG}
 status_check
 
-rabbitmqctl add_user roboshop roboshop123 &>>${LOG}
+print_head "Create User and Password"
+rabbitmqctl list_users | grep roboshop &>>${LOG}
+if [ $? -ne 0 ]; then
+  rabbitmqctl add_user roboshop ${roboshop_rabbitmq_password} &>>${LOG}
+fi
 status_check
+
+print_head "Add Tags To User"
 rabbitmqctl set_user_tags roboshop administrator &>>${LOG}
 status_check
+
+print_head "Set Permissions To User"
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>${LOG}
 status_check
 
